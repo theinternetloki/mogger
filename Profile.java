8f724959d930e7d039d8f540c369346e477eeafc
theinternetloki <michaelsahion@gmail.com>
2013-02-01 14:02:14 -0800

    Create Profile.java

---
+++ Profile.java
@@ -0,0 +1,16 @@
+package com.moggerinc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Profile extends Activity {
+
+  /** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.profile);
+	    // TODO Auto-generated method stub
+	}
+
+}

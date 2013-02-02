e8fd9e4040e23d53b3b0746f814fa01cf901e379
Abanoub George <abanoub14@gmail.com>
2013-02-02 13:51:58 -0600

    login and registration
    Fixed the login and registration screens still need to link them to the
    php files for login and registration

--- gen/com/kimistudios/mogger/R.java
+++ gen/com/kimistudios/mogger/R.java
@@ -15,9 +15,11 @@ public final class R {
     }
     public static final class drawable {
         public static final int bg=0x7f020000;
-        public static final int brown=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int login=0x7f020003;
+        public static final int bglogin=0x7f020001;
+        public static final int bgreg=0x7f020002;
+        public static final int brown=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int login=0x7f020005;
     }
     public static final class id {
         public static final int Login=0x7f080005;
@@ -35,7 +37,8 @@ public final class R {
     public static final class layout {
         public static final int activity_login=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int activity_register=0x7f030002;
+        public static final int activity_profile=0x7f030002;
+        public static final int activity_register=0x7f030003;
     }
     public static final class menu {
         public static final int activity_main=0x7f070000;
---
+++ src/com/kimistudios/mogger/Profile.java
@@ -0,0 +1,17 @@
+package com.kimistudios.mogger;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Profile extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_profile);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}

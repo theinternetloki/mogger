5c9c1e56578c5fc5bc0e88b07a84a3d4ccd309cd
Abanoub George <abanoub14@gmail.com>
2013-02-06 19:21:45 -0600

    merged all source together
    download this source and we will both work on it
    tell me if you want to change the name to com.moggerinc or just leave it
    with com.kimistudios.mogger

--- gen/com/kimistudios/mogger/R.java
+++ gen/com/kimistudios/mogger/R.java
@@ -20,20 +20,28 @@ public final class R {
         public static final int brown=0x7f020003;
         public static final int ic_launcher=0x7f020004;
         public static final int login=0x7f020005;
+        public static final int moggerprofilepic=0x7f020006;
     }
     public static final class id {
         public static final int Login=0x7f080005;
         public static final int Register=0x7f080004;
         public static final int RelativeLayout1=0x7f080000;
         public static final int button1=0x7f080003;
+        public static final int button2=0x7f08000c;
+        public static final int button3=0x7f08000d;
+        public static final int button4=0x7f08000a;
         public static final int editText1=0x7f080001;
         public static final int editText2=0x7f080002;
-        public static final int editText3=0x7f08000a;
-        public static final int editText4=0x7f080008;
-        public static final int editText5=0x7f080009;
+        public static final int editText3=0x7f080011;
+        public static final int editText4=0x7f08000f;
+        public static final int editText5=0x7f080010;
         public static final int imageView1=0x7f080006;
-        public static final int menu_settings=0x7f08000b;
-        public static final int webView1=0x7f080007;
+        public static final int menu_settings=0x7f080012;
+        public static final int quickContactBadge1=0x7f080007;
+        public static final int textView1=0x7f080009;
+        public static final int textView2=0x7f080008;
+        public static final int textView3=0x7f08000b;
+        public static final int webView1=0x7f08000e;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
@@ -46,17 +54,24 @@ public final class R {
     }
     public static final class string {
         public static final int FirstLast=0x7f050009;
+        public static final int Friends=0x7f050010;
         public static final int Login=0x7f050004;
         public static final int Loginimg=0x7f050005;
         public static final int Password=0x7f050008;
         public static final int Register=0x7f050003;
         public static final int Submit=0x7f050007;
+        public static final int Textview=0x7f05000d;
         public static final int app_name=0x7f050000;
+        public static final int bio=0x7f050013;
+        public static final int edit=0x7f05000e;
         public static final int email=0x7f05000a;
         public static final int hello_world=0x7f050001;
         public static final int menu_settings=0x7f050002;
         public static final int nothing=0x7f05000c;
+        public static final int post=0x7f050012;
+        public static final int upload=0x7f05000f;
         public static final int username=0x7f050006;
+        public static final int wgn=0x7f050011;
         public static final int year=0x7f05000b;
     }
     public static final class style {
--- src/com/kimistudios/mogger/Profile.java
+++ src/com/kimistudios/mogger/Profile.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class Profile extends Activity {
 
@@ -13,10 +14,11 @@ public class Profile extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_profile);
-	    WebView mywebview = (WebView) findViewById(R.id.webView1);
-		 mywebview.loadUrl("http://www.stmgang.com/app/feed.html");
-		 WebSettings webSettings = mywebview.getSettings();
-		 webSettings.setJavaScriptEnabled(true);
+		WebView mywebview = (WebView) findViewById(R.id.webView1);
+        mywebview.loadUrl("http://www.stmgang.com/app/posts.html");
+        WebSettings webSettings = mywebview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+		mywebview.setWebViewClient(new WebViewClient());
 	
 	    // TODO Auto-generated method stub
 	}

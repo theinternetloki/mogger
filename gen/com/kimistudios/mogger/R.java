001e790aefb544ab888892da2f7e9a01f040f004
Abanoub George <abanoub14@gmail.com>
2013-02-06 21:10:41 -0600

    deleted unnecessary stuffs

--- gen/com/kimistudios/mogger/R.java
+++ gen/com/kimistudios/mogger/R.java
@@ -23,19 +23,19 @@ public final class R {
         public static final int moggerprofilepic=0x7f020006;
     }
     public static final class id {
-        public static final int Login=0x7f080005;
-        public static final int Register=0x7f080004;
+        public static final int Login=0x7f080006;
+        public static final int Register=0x7f080005;
         public static final int RelativeLayout1=0x7f080000;
         public static final int button1=0x7f080003;
         public static final int button2=0x7f08000c;
         public static final int button3=0x7f08000d;
         public static final int button4=0x7f08000a;
-        public static final int editText1=0x7f080001;
-        public static final int editText2=0x7f080002;
+        public static final int editText1=0x7f080002;
+        public static final int editText2=0x7f080001;
         public static final int editText3=0x7f080011;
         public static final int editText4=0x7f08000f;
         public static final int editText5=0x7f080010;
-        public static final int imageView1=0x7f080006;
+        public static final int imageView1=0x7f080004;
         public static final int menu_settings=0x7f080012;
         public static final int quickContactBadge1=0x7f080007;
         public static final int textView1=0x7f080009;

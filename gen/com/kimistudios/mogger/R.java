9bf0b1c619e91ea8ed08453e8e697963b2ae293f
Abanoub George <abanoub14@gmail.com>
2013-02-08 23:16:36 -0600

    made small changes
    off to bed :P

--- gen/com/kimistudios/mogger/R.java
+++ gen/com/kimistudios/mogger/R.java
@@ -15,12 +15,9 @@ public final class R {
     }
     public static final class drawable {
         public static final int bg=0x7f020000;
-        public static final int bglogin=0x7f020001;
-        public static final int bgreg=0x7f020002;
-        public static final int brown=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int login=0x7f020005;
-        public static final int moggerprofilepic=0x7f020006;
+        public static final int brown=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int moggerprofilepic=0x7f020003;
     }
     public static final class id {
         public static final int Login=0x7f080006;
@@ -32,9 +29,9 @@ public final class R {
         public static final int button4=0x7f08000a;
         public static final int editText1=0x7f080002;
         public static final int editText2=0x7f080001;
-        public static final int editText3=0x7f080011;
-        public static final int editText4=0x7f08000f;
-        public static final int editText5=0x7f080010;
+        public static final int editText3=0x7f08000f;
+        public static final int editText4=0x7f080010;
+        public static final int editText5=0x7f080011;
         public static final int imageView1=0x7f080004;
         public static final int menu_settings=0x7f080012;
         public static final int quickContactBadge1=0x7f080007;
